f0f2fdae1b8b1fbe6fa0b528a1976a1490f1f4ff
Harrison de Jong <harrisondijon@gmail.com>
2024-10-13 00:12:16 -0700

    Filled out reqeust handling in server.js and moved user-facing strings and messaged to user.js

---
+++ lang/messages/en/user.js
@@ -0,0 +1,9 @@
+export const MESSAGES = {
+    noWord: 'Error, no word in query',
+    wordNotFound: '%WORD% not found!',
+    invalidInput: 'Error: Invalid input. Word and definition must be non-empty strings',
+    newEntry: 'New Entry Recorded',
+    alreadyExists: 'Warning! The word %WORD% already exists.',
+    invalidJSON: 'Error: Invalid JSON input',
+    routeNotFound: 'Error: Route not found.',
+}
--- server.js
+++ server.js
@@ -1,51 +1,108 @@
-const http = require('http');
-const url = require('url');
-import MESSAGES from './lang/messages/en/user.js'
-const Dictionary = require('./modules/dictionary');
+const http = require("http");
+const url = require("url");
+import MESSAGES from "./lang/messages/en/user.js";
+const Dictionary = require("./modules/dictionary");
 
-const PORT = 3000;
+const PORT = 3004;
 const dictionary = new Dictionary();
+let totalRequests = 0;
 
 const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const pathname = parsedUrl.pathname;
+  const query = parsedUrl.query;
 
-    const parsedUrl =  url.parse(req.url, true);
-    const pathname = parsedUrl.pathname;
-    const query = parsedUrl.query;
+  res.setHeader("Content-Type", "application.JSON");
+  res.setHeader("Access-control-Allow-Origin", "*");
 
-    //getting a word from the dictionary
-    if (pathname='/add/definitions' && req.method =='GET'){
-        const word = query.word;
+  if ((pathname = "/add/definitions" && req.method == "GET")) {
+    const word = query.word;
 
-        //check is there is a word send
-        if (!word){
-
-            res.writeHead(400);
-            res.end(JSON.stringify({message: MESSAGES.noWord}))
-        }
-
-        // retrieve the word
-        dictionary.retrieveDefinition(word);
-
-        // set the headers and send back the word and the definition
-
-        // 
-    
-    // adding a word to the dictionary
-    }else if (pathname='/add/definitions' && req.method =='GET'){
-
-        //increment the number of requests sent
-
-        //attempt to add the word
-        if (isInDictionary(word)){
-
-            //prep and send back a response
-
-        } else{
-            //add the word, this increments the counter and should return a value
-
-            // send back the response 
-
-
-        }
+    if (!word) {
+      res.writeHead(400);
+      res.end(JSON.stringify({ message: MESSAGES.noWord }));
+      return;
     }
-})
+
+    const definition = dictionary.retrieveDefinition(word);
+
+    if (definition) {
+      res.writeHead(200);
+      res.end(
+        JSON.stringify({
+          requestNumber: totalRequests,
+          wordCound: dictionary.getWordCount(),
+          word: word,
+          definition: definition,
+        })
+      );
+    } else {
+      res.writeHead(404);
+      res.end(
+        JSON.stringify({
+          requestNumber: totalRequests,
+          wordCound: dictionary.getWordCount(),
+          word: word,
+          message: MESSAGES.wordNotFound.replace("%WORD%", word),
+        })
+      );
+    }
+
+    // adding a word to the dictionary
+  } else if ((pathname = "/api/definitions" && req.method == "POST")) {
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on("end", () => {
+      try {
+        const data = JSON.parse(body);
+        const word = data.word;
+        const definition = data.definition;
+
+        if (
+          !word ||
+          !definition ||
+          typeof word !== "string" ||
+          typeof definition !== "string"
+        ) {
+          res.writeHead(400);
+          res.end(JSON.stringify({ message: MESSAGES.invalidInput }));
+          return;
+        }
+
+        const result = dictionary.addWord(word, definition);
+
+        if (result.success) {
+          res.writeHead(201);
+          res.end(
+            JSON.stringify({
+              requestNumber: totalRequests,
+              message: MESSAGES.newEntry,
+              totalEntries: dictionary.getTotalEntries(),
+            })
+          );
+        } else {
+          res.writeHead(400);
+          res.end(
+            JSON.stringify({
+              requestNumber: totalRequests,
+              message: MESSAGES.alreadyExists.replace("%WORD%", word),
+              totalEntries: dictionary.getTotalEntries(),
+            })
+          );
+        }
+      } catch (error) {
+        res.writeHead(400);
+        res.end(JSON.stringify({ message: MESSAGES.invalidJSON }));
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end(JSON.stringify({ message: MESSAGES.routeNotFound }));
+  }
+});
+
+server.listen(PORT);
