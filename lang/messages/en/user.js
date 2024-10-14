190345e0bca3c0b432a01e7e57d825bbd5dc62a4
Harrison de Jong <harrisondijon@gmail.com>
2024-10-13 20:59:46 -0700

    Change ES6 syntax on export from user.js to Commonjs

--- lang/messages/en/user.js
+++ lang/messages/en/user.js
@@ -1,9 +1,11 @@
-export const MESSAGES = {
+const MESSAGES = {
     noWord: 'Error, no word in query',
     wordNotFound: '%WORD% not found!',
     invalidInput: 'Error: Invalid input. Word and definition must be non-empty strings',
     newEntry: 'New Entry Recorded',
     alreadyExists: 'Warning! The word %WORD% already exists.',
     invalidJSON: 'Error: Invalid JSON input',
-    routeNotFound: 'Error: Route not found.',
-}
+    routeNotFound: 'Error: Route not found.'
+};
+
+module.exports = MESSAGES;
